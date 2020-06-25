ce4b72051af4a879a816d31b83877bc141cf4772
TienNDSE62860 <44231091+TienNDSE62860@users.noreply.github.com>
2020-06-26 03:04:12 +0700

    update car

--- src/app/main/booking/CarEdit.js
+++ src/app/main/booking/CarEdit.js
@@ -1,166 +1,286 @@
-import React from 'react'
-import { Button, TextField, Card, Typography, Grid, makeStyles } from '@material-ui/core';
-import PublishIcon from '@material-ui/icons/Publish';
+import React, { useState } from "react";
+import {
+  Button,
+  TextField,
+  Card,
+  Typography,
+  Grid,
+  makeStyles,
+  Switch,
+} from "@material-ui/core";
+import PublishIcon from "@material-ui/icons/Publish";
 import ImageUploading from "react-images-uploading";
-import DriveEtaIcon from '@material-ui/icons/DriveEta';
-import AccountBoxIcon from '@material-ui/icons/AccountBox';
-import EditIcon from '@material-ui/icons/Edit';
-import DeleteIcon from '@material-ui/icons/Delete';
-import InputAdornment from '@material-ui/core/InputAdornment';
-import Layout from '../../layout';
+import DriveEtaIcon from "@material-ui/icons/DriveEta";
+import EditIcon from "@material-ui/icons/Edit";
+import DeleteIcon from "@material-ui/icons/Delete";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import Layout from "../../layout";
+import { withStyles } from "@material-ui/core/styles";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
 
 const ITEM_HEIGHT = 48;
-const useStyles = makeStyles(theme => ({
-    root: {
-        color: theme.palette.primary.contrastText
-    },
-    media: {
-        height: 140,
-        width: '100%'
-    },
-    imageUploading: {
-        height: 100
-    },
-    icon: {
-        height: '100%',
-        width: 50,
-        marginRight: 10
-    },
-    formControl: {
-        width: '100%',
-        maxHeight: ITEM_HEIGHT * 4.5,
-    },
-    textField: {
-        width: '100%'
-    },
-    card: {
-        margin: 20,
-        padding: 20
-    },
-
-
+const useStyles = makeStyles((theme) => ({
+  root: {
+    color: theme.palette.primary.contrastText,
+  },
+  media: {
+    height: 140,
+    width: "100%",
+  },
+  imageUploading: {
+    height: 100,
+  },
+  icon: {
+    height: "100%",
+    width: 50,
+    marginRight: 10,
+  },
+  formControl: {
+    width: "100%",
+    maxHeight: ITEM_HEIGHT * 4.5,
+  },
+  textField: {
+    width: "100%",
+    margin: theme.spacing(1),
+  },
+  card: {
+    margin: 20,
+    padding: 20,
+  },
+  switchButton: {
+    marginLeft: theme.spacing(2),
+  },
 }));
 
 export default function CarEdits(props) {
-    // const [brand, setBrand] = React.useState('');
-    // const [model, setModel] = React.useState('');
-    const maxNumber = 10;
-    const maxMbFileSize = 5 * 1024 * 1024; // 5Mb
+  // const [brand, setBrand] = React.useState('');
+  // const [model, setModel] = React.useState('');
+  const maxNumber = 10;
+  const maxMbFileSize = 5 * 1024 * 1024; // 5Mb
 
-    const classes = useStyles();
+  const classes = useStyles();
+
+  const [state, setState] = useState({
+    checkedAvailable: true,
+  });
+
+  const handleChange = (event) => {
+    setState({
+      state,
+      [event.target.name]: event.target.checked,
+    });
+  };
+
+  const IOSSwitch = withStyles((theme) => ({
+    root: {
+      width: 42,
+      height: 26,
+      padding: 0,
+      margin: theme.spacing(1),
+    },
+    switchBase: {
+      padding: 1,
+      "&$checked": {
+        transform: "translateX(16px)",
+        color: theme.palette.common.white,
+        "& + $track": {
+          backgroundColor: "#52d869",
+          opacity: 1,
+          border: "none",
+        },
+      },
+      "&$focusVisible $thumb": {
+        color: "#52d869",
+        border: "6px solid #fff",
+      },
+    },
+    thumb: {
+      width: 24,
+      height: 24,
+    },
+    track: {
+      borderRadius: 26 / 2,
+      border: `1px solid ${theme.palette.grey[400]}`,
+      backgroundColor: theme.palette.grey[50],
+      opacity: 1,
+      transition: theme.transitions.create(["background-color", "border"]),
+    },
+    checked: {},
+    focusVisible: {},
+  }))(({ classes, ...props }) => {
     return (
-        <Layout name="Vinfast SA 2.0">
-            <Grid container>
-                <Grid item xs={12} sm={6} >
-                    <Card className={classes.card}>
-                        <Typography>Tell us a bit about car owner</Typography>
-                        <TextField className={classes.textField} label="Full name" value="Tran Duc Thai" />
-                        <TextField className={classes.textField} label="Mobile number" value="0348130639" />
-                        <TextField className={classes.textField} label="Email" value="thaitdse62593@fpt.edu.vn" />
-                        <TextField className={classes.textField} label="Identity Card" value="272752339" />
-                        <div className="mt-20">
-                            <ImageUploading
-                                onChange={classes.onChange}
-                                maxNumber={maxNumber}
-                                multiple
-                                maxFileSize={maxMbFileSize}
-                                acceptType={["jpg", "gif", "png"]}
-                            >
-                                {
-                                    ({ imageList, onImageUpload }) => (
-                                        <div>
-                                            <Button variant="contained" color="primary" onClick={onImageUpload} component="span" startIcon={<AccountBoxIcon />}>
-                                                Upload Image
-                                                </Button>
-                                            {
-                                                imageList.map((image) => (
-                                                    <div key={image.key} className="mt-20">
-                                                        <Grid container spacing={1}>
-                                                            <Grid item xs={9} sm={9} >
-                                                                <img alt="" src={image.dataURL} className={classes.imageUploading} />
-                                                            </Grid>
-                                                            <Grid item xs={3} sm={3} >
-                                                                <Button startIcon={<EditIcon />} onClick={image.onUpdate}>Update</Button>
-                                                                <Button startIcon={<DeleteIcon />} onClick={image.onRemove}>Delete</Button>
-                                                            </Grid>
-                                                        </Grid>
-                                                    </div>
-                                                ))
-                                            }
-                                        </div>
-                                    )}
-                            </ImageUploading>
-                        </div>
-                    </Card>
-                </Grid>
-                <Grid item xs={12} sm={6} >
-                    <Card className={classes.card}>
-                        <Typography>Tell us a bit about your car</Typography>
-                        <TextField
-                            id="input-with-icon-textfield"
-                            label="Brand"
-                            className={classes.textField}
-                            value="Vinfast"
-                            disabled
-                            InputProps={{
-                                startAdornment: (
-                                    <img alt="" className={classes.icon} src="https://static.carmudi.vn/wp-content/uploads/2016/04/Honda-Carmudi.jpg" />
-                                ),
-                            }}
-                        />
-                        <TextField
-                            id="input-with-icon-textfield"
-                            label="Model"
-                            value="Lux SA 2.0"
-                            className={classes.textField}
-                            disabled
-                        />
+      <Switch
+        focusVisibleClassName={classes.focusVisible}
+        disableRipple
+        classes={{
+          root: classes.root,
+          switchBase: classes.switchBase,
+          thumb: classes.thumb,
+          track: classes.track,
+          checked: classes.checked,
+        }}
+        {...props}
+      />
+    );
+  });
 
-                        <TextField className={classes.textField} id="standard-required" value="60B2-02914" label="Car Registration number" />
-                        <TextField className={classes.textField} id="standard-required" value="23456" label="Odometer" InputProps={{
-                            startAdornment: <InputAdornment position="start">KM</InputAdornment>,
-                        }} />
-                        <div className="mt-20">
-                            <ImageUploading
-                                onChange={classes.onChange}
-                                maxNumber={maxNumber}
-                                multiple
-                                maxFileSize={maxMbFileSize}
-                                acceptType={["jpg", "gif", "png"]}
-                            >
-                                {
-                                    ({ imageList, onImageUpload }) => (
-                                        <div>
-                                            <Button variant="contained" color="primary" onClick={onImageUpload} component="span" startIcon={<DriveEtaIcon />}>
-                                                Upload Image
-                                                </Button>
-                                            {
-                                                imageList.map((image) => (
-                                                    <div key={image.key} className="mt-20">
-                                                        <Grid container spacing={1}>
-                                                            <Grid item xs={9} sm={9} >
-                                                                <img alt="" src={image.dataURL} className={classes.imageUploading} />
-                                                            </Grid>
-                                                            <Grid item xs={3} sm={3} >
-                                                                <Button startIcon={<EditIcon />} onClick={image.onUpdate}>Update</Button>
-                                                                <Button startIcon={<DeleteIcon />} onClick={image.onRemove}>Delete</Button>
-                                                            </Grid>
-                                                        </Grid>
-                                                    </div>
-                                                ))
-                                            }
-                                        </div>
-                                    )}
-                            </ImageUploading>
-                        </div>
-                    </Card>
-                </Grid>
-                <Grid container justify="center" >
-                    <Button variant="contained" color="primary" startIcon={<PublishIcon />}>
-                        Finish
+  return (
+    <Layout name="Vinfast SA 2.0">
+      <Grid container justify="center">
+        <Grid item xs={12} sm={6} lg={10}>
+          <Card className={classes.card}>
+            <TextField
+              id="input-with-icon-textfield"
+              label="Brand"
+              className={classes.textField}
+              value="Vinfast"
+              disabled
+              variant="outlined"
+              InputProps={{
+                startAdornment: (
+                  <img
+                    alt=""
+                    className={classes.icon}
+                    src="https://static.carmudi.vn/wp-content/uploads/2016/04/Honda-Carmudi.jpg"
+                  />
+                ),
+              }}
+            />
+
+            <TextField
+              id="input-with-icon-textfield"
+              label="Model"
+              variant="outlined"
+              value="Lux SA 2.0"
+              className={classes.textField}
+              disabled
+            />
+
+            <TextField
+              className={classes.textField}
+              id="standard-required"
+              value="60B2-02914"
+              label="Name"
+              variant="outlined"
+              disabled
+            />
+
+            <FormControlLabel
+              classes={classes.switchButton}
+              control={
+                <IOSSwitch
+                  checked={state.checkedAvailable}
+                  onChange={handleChange}
+                  name="checkedAvailable"
+                />
+              }
+              label="Available"
+            />
+            <div className="mt-20">
+              <ImageUploading
+                onChange={classes.onChange}
+                maxNumber={maxNumber}
+                multiple
+                maxFileSize={maxMbFileSize}
+                acceptType={["jpg", "gif", "png"]}
+              >
+                {({ imageList, onImageUpload }) => (
+                  <div>
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      onClick={onImageUpload}
+                      component="span"
+                      startIcon={<DriveEtaIcon />}
+                    >
+                      Change Image
                     </Button>
-                </Grid>
-            </Grid>
-        </Layout>
-    )
+                    {imageList.map((image) => (
+                      <div key={image.key} className="mt-20">
+                        <Grid container spacing={1}>
+                          <Grid item xs={9} sm={9}>
+                            <img
+                              alt=""
+                              src={image.dataURL}
+                              className={classes.imageUploading}
+                            />
+                          </Grid>
+                          <Grid item xs={3} sm={3}>
+                            <Button
+                              startIcon={<EditIcon />}
+                              onClick={image.onUpdate}
+                            >
+                              Update
+                            </Button>
+                            <Button
+                              startIcon={<DeleteIcon />}
+                              onClick={image.onRemove}
+                            >
+                              Delete
+                            </Button>
+                          </Grid>
+                        </Grid>
+                      </div>
+                    ))}
+                  </div>
+                )}
+              </ImageUploading>
+            </div>
+          </Card>
+        </Grid>
+
+        <Grid item xs={12} sm={6} lg={10}>
+          <Card className={classes.card}>
+            <TextField
+              className={classes.textField}
+              id="standard-required"
+              value="60B2-02914"
+              label="Vin"
+              variant="outlined"
+            />
+
+            <TextField
+              className={classes.textField}
+              id="standard-required"
+              value="60B2-02914"
+              label="Seat"
+              variant="outlined"
+            />
+
+            <TextField
+              className={classes.textField}
+              id="standard-required"
+              value="60B2-02914"
+              label="Sound"
+              variant="outlined"
+            />
+
+            <TextField
+              className={classes.textField}
+              id="standard-required"
+              value="60B2-02914"
+              label="Price"
+              variant="outlined"
+            />
+            <TextField
+              className={classes.textField}
+              id="standard-required"
+              value="60B2-02914"
+              label="Plate number"
+              variant="outlined"
+            />
+          </Card>
+        </Grid>
+        <Grid container justify="center">
+          <Button
+            variant="contained"
+            color="primary"
+            startIcon={<PublishIcon />}
+          >
+            Update
+          </Button>
+        </Grid>
+      </Grid>
+    </Layout>
+  );
 }
