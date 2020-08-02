9486c88ee36edf59f40ab274572bde834db465a5
longdbse62792 <nhacsisitinh@gmail.com>
2020-08-02 12:12:11 +0700

    -add component submit review booking

--- src/app/main/booking/booking.reducer.js
+++ src/app/main/booking/booking.reducer.js
@@ -13,6 +13,7 @@ const initialState = {
   agreemnts: [],
   booking: {},
   change: false,
+  disableButton: false,
 };
 
 const bookingReducer = (state = initialState, { type, payload }) => {
@@ -129,6 +130,7 @@ const bookingReducer = (state = initialState, { type, payload }) => {
         ...state,
         loading: false,
         reviews: [...state.reviews, payload],
+        disableButton: true,
       };
     }
     case Actions.POST_REVIEW_SUBMIT: {
